134f20aad9bc1dbe89b27305183446400269b175
Ray Toal <rtoal@lmu.edu>
2023-09-06 07:43:52 -0700

    Tighten up generator code

---
+++ generator.js
@@ -0,0 +1,67 @@
+import * as core from "./core.js"
+
+export default function generate(program) {
+  // Variable names in JS will be suffixed with _1, _2, _3, etc. This is
+  // because "for", for example, is a legal variable name in Bella, but
+  // not in JS. So we want to generate something like "for_1". We handle
+  // this by mapping each variable declaration to its suffix.
+  const mapping = new Map()
+  return program.gen({
+    targetName(entity) {
+      if (!mapping.has(entity)) {
+        mapping.set(entity, mapping.size + 1)
+      }
+      return `${entity.name}_${mapping.get(entity)}`
+    },
+  })
+}
+
+core.Program.prototype.gen = function (c) {
+  return this.statements.gen(c).join("\n")
+}
+core.VariableDeclaration.prototype.gen = function (c) {
+  return `let ${this.variable.gen(c)} = ${this.initializer.gen(c)};`
+}
+core.Variable.prototype.gen = function (c) {
+  return c.targetName(this)
+}
+core.FunctionDeclaration.prototype.gen = function (c) {
+  const params = this.params.map((n) => c.targetName(n)).join(", ")
+  return `function ${this.fun.gen(c)}(${params}) { return ${this.body.gen(
+    c
+  )}; }`
+}
+core.Function.prototype.gen = function (c) {
+  return c.targetName(this)
+}
+core.PrintStatement.prototype.gen = function (c) {
+  return `console.log(${this.argument.gen(c)});`
+}
+core.Assignment.prototype.gen = function (c) {
+  return `${this.target.gen(c)} = ${this.source.gen(c)};`
+}
+core.WhileStatement.prototype.gen = function (c) {
+  return [`while (${this.test.gen(c)}) {`, ...this.body.gen(c), "}"].join("\n")
+}
+core.Call.prototype.gen = function (c) {
+  return `${this.callee.gen(c)}(${this.args.gen(c)})`
+}
+core.Conditional.prototype.gen = function (c) {
+  const { test, consequent, alternate } = this
+  return `((${test.gen(c)}) ? (${consequent.gen(c)}) : (${alternate.gen(c)}))`
+}
+core.BinaryExpression.prototype.gen = function (c) {
+  return `(${this.left.gen(c)} ${this.op} ${this.right.gen(c)})`
+}
+core.UnaryExpression.prototype.gen = function (c) {
+  return `${this.op}(${this.operand.gen(c)})`
+}
+Number.prototype.gen = function (c) {
+  return this
+}
+Boolean.prototype.gen = function (c) {
+  return this
+}
+Array.prototype.gen = function (c) {
+  return this.map((element) => element.gen(c))
+}
--- src/bella.js
+++ src/bella.js
@@ -11,14 +11,13 @@ export function compile(source) {
   return generate(optimize(analyze(parse(source))))
 }
 
-if (process.argv.length !== 3) {
-  console.error("Must have exactly one argument, a file name")
-} else {
-  try {
-    const buffer = await fs.readFile(process.argv[2])
-    console.log(compile(buffer.toString()))
-  } catch (e) {
-    console.error(`\u001b[31m${e}\u001b[39m`)
-    process.exitCode = 1
+try {
+  if (process.argv.length !== 3) {
+    throw new Error("Must have exactly one argument, a file name")
   }
+  const buffer = await fs.readFile(process.argv[2])
+  console.log(compile(buffer.toString()))
+} catch (e) {
+  console.error(`\u001b[31m${e}\u001b[39m`)
+  process.exitCode = 1
 }
