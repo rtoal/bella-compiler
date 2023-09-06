be811a5d536c7b502930be332b0fde119e4e00a9
Ray Toal <rtoal@lmu.edu>
2023-09-05 22:18:01 -0700

    Split grammar

--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,11 @@
 import * as fs from "node:fs"
 import * as ohm from "ohm-js"
 
-const grammar = ohm.grammar(fs.readFileSync("src/bella.ohm"))
+const grammar = ohm.grammar(`Bella {
+  ${fs.readFileSync("src/phrase_grammar.ohm")}
+  ${fs.readFileSync("src/lexical_grammar.ohm")}
+}`)
 
-// Returns the Ohm match if successful, otherwise throws an error
 export default function parse(sourceCode) {
   const match = grammar.match(sourceCode)
   if (!match.succeeded()) throw new Error(match.message)
