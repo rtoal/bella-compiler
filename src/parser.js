4a708cedb8e76bdc1de56a39dc6987d18bc80713
Ray Toal <rtoal@lmu.edu>
2023-09-07 21:15:32 -0700

    Combine grammars

--- src/parser.js
+++ src/parser.js
@@ -1,10 +1,7 @@
 import * as fs from "node:fs"
 import * as ohm from "ohm-js"
 
-const grammar = ohm.grammar(`Bella {
-  ${fs.readFileSync("src/phrase_grammar.ohm")}
-  ${fs.readFileSync("src/lexical_grammar.ohm")}
-}`)
+const grammar = ohm.grammar(fs.readFileSync("src/bella.ohm"))
 
 export default function parse(sourceCode) {
   const match = grammar.match(sourceCode)
